f356755f191ecb351ddc972b769144d455dd7e0b
Fernando Ludvig <fernandoludvig0804@gmail.com>
2025-10-03 18:55:20 -0300

    fix: Swagger agora usa URL correta do Render em produção
    - Adicionada URL de produção como primeira opção
    - Swagger agora faz requisições para o servidor correto
    - Corrige erro 'Failed to fetch' ao executar operações

--- finance-tracker/src/docs/swagger.js
+++ finance-tracker/src/docs/swagger.js
@@ -14,6 +14,10 @@ const options = {
     },
     servers: [
       {
+        url: 'https://cse341-project1-fphm.onrender.com/finance-tracker',
+        description: 'Servidor de produção'
+      },
+      {
         url: 'http://localhost:3000',
         description: 'Servidor de desenvolvimento'
       }
